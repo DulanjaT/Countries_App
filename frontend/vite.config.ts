5d2ede1ee6882affb0e5351c104c525c1a2cf60d
Dulanja Gunawardena <dulanja.coding@gmail.com>
2025-05-27 04:50:22 +0300

    added correct netlify and vite config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   server: {
     port: 5180,
   },
